b48d365a6bb35197a0248ce1d33a3fa2024b73da
Mathews P Mathew <mathewsparan@gmail.com>
2021-12-15 23:27:42 +0530

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Scanner;
 
 
-public class Matrix {
+public class Main {
 public static void main(String args[])
 {
 	int a[][]=new int[10][10];
@@ -34,34 +34,54 @@ n1=sc.nextInt();
 System.out.println("row elements");
 for(i=0;i<m1;i++) {
 	for(j=0;j<n1;j++) {
+	    System.out.println("["+i+"]"+"["+j+"}");
 		a[i][j]=sc.nextInt();
 		}}
+		System.out.println("Entred firt matrix is: ");
+		for(i=0;i<m1;i++)
+		{
+		    for(j=0;j<n1;j++){
+		        System.out.print(a[i][j]+" ");
+		    }
+		    System.out.println();
+		    
+		}
 		
 System.out.println("For the second matrix");
-System.out.println("enter the row and colom of first matrix");
+System.out.println("enter the row and colom of second matrix");
 m2=sc.nextInt();
 n2=sc.nextInt();
 System.out.println("row elements");
 for(i=0;i<m2;i++) {
 	for(j=0;j<n2;j++) {
+	    System.out.println("["+i+"]"+"["+j+"}");
 		b[i][j]=sc.nextInt();
 
 }
 }
+System.out.println("Entred second matrix is: ");
+		for(i=0;i<m1;i++)
+		{
+		    for(j=0;j<n1;j++){
+		        System.out.print(a[i][j]+" ");
+		    }
+		    System.out.println();
+}
+
 int multi=0;
 	for(i=0;i<m1;i++) {
 		for(j=0;j<n2;j++) {
-			for(k=0;k<n1;k++) {
-				multi=multi+(a[i][k]*b[k][j]);
+			for(k=0;k<m2;k++) {
+				c[i][j]=c[i][j]+(a[i][k]*b[k][j]);
 			}
-			c[i][j]=multi;
+			//c[i][j]=multi;
 		}
 	}
 	System.out.println("the multiplied matrix");
 	
 	for(i=0;i<m1;i++) {
 		for(j=0;j<n2;j++) {
-			System.out.print(c[i][j]);
+			System.out.print(c[i][j]+" ");
 			
 		}
 		System.out.println();
